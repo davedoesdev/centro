ba66063c4a9be71aae45a106b062e460ff7a7aa6
David Halls <dahalls@gmail.com>
2016-08-15 23:14:10 +0100

    Add ready event

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = function (grunt)
+{
+    grunt.initConfig(
+    {
+        jshint: {
+            src: [ 'index.js', 'Gruntfile.js', 'lib/**/*.js', 'test/**/*.js' ],
+            options: {
+                node: true,
+                esversion: 6
+            }
+        },
+
+        mochaTest: {
+            src: [ 'test/in-mem.js', 'test/tcp.js', 'test/example/example.js' ],
+            options: {
+                bail: true
+            }
+        },
+
+        apidox: {
+            input: [ 'index.js', 'events_doc.js' ],
+            output: 'README.md',
+            fullSourceDescription: true,
+            extraHeadingLevels: 1
+        },
+
+        shell: {
+            cover: {
+                command: './node_modules/.bin/istanbul cover -x Gruntfile.js ./node_modules/.bin/grunt -- test',
+                execOptions: {
+                    maxBuffer: 10000 * 1024
+                }
+            },
+
+            check_cover: {
+                command: './node_modules/.bin/istanbul check-coverage --statement 100 --branch 100 --function 100 --line 100'
+            },
+
+            coveralls: {
+                command: 'cat coverage/lcov.info | coveralls'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-apidox');
+
+    grunt.registerTask('lint', 'jshint');
+    grunt.registerTask('test', 'mochaTest');
+    grunt.registerTask('docs', ['apidox']);
+    grunt.registerTask('coverage', ['shell:cover', 'shell:check_cover']);
+    grunt.registerTask('coveralls', 'shell:coveralls');
+    grunt.registerTask('default', ['lint', 'test']);
+};
+
