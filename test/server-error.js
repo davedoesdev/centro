9a759c485fc5f5829f90537115d77b94ae836e73
David Halls <dahalls@gmail.com>
2016-10-10 23:08:19 +0100

    Add test for transport error with per-transport authz config

--- test/server-error.js
+++ test/server-error.js
@@ -26,6 +26,29 @@ describe('server errors', function ()
         });
     });
 
+    it('should pass back run transport errors (auth config)', function (done)
+    {
+        var server = new CentroServer(
+        {
+            authorize: require('authorize-jwt'),
+            db_type: 'pouchdb',
+            db_for_update: true,
+            transport: {
+                server: function (config, authorize, connected, ready, error, warning)
+                {
+                    ready(new Error('dummy'));
+                },
+                authorize_config: {}
+            }
+        });
+
+        server.on('error', function (err)
+        {
+            expect(err.message).to.equal('dummy');
+            done();
+        });
+    });
+
     it('should pass back create authorizer errors', function (done)
     {
         var server = new CentroServer(
