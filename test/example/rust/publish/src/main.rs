dc8e9108a7330531d72ccce119d89c7462bedb6d
David Halls <dahalls@gmail.com>
2017-05-22 23:59:31 +0100

    Rust example: log publish response if error

--- test/example/rust/publish/src/main.rs
+++ test/example/rust/publish/src/main.rs
@@ -1,9 +1,12 @@
 extern crate reqwest;
-use std::io;
+use std::io::{self, Read};
 use std::env;
 use reqwest::{Url, Client};
+#[macro_use] extern crate log;
+extern crate env_logger;
 
 fn main() {
+    env_logger::init().expect("Failed to init logger");
     let url_str = "http://localhost:8802/centro/v1/publish";
     let token = env::var("CENTRO_TOKEN").expect("no token");
     let topic = env::args().nth(1).expect("no topic");
@@ -11,10 +14,15 @@ fn main() {
         ("authz_token", token),
         ("topic", topic)])
         .expect("Failed to parse url");
-    let mut response = Client::new().expect("Couldn't create client")
+    let response = Client::new().expect("Couldn't create client")
         .post(url)
         .body(reqwest::Body::new(io::stdin()))
         .send()
         .expect("Failed to send request");
-    let _ = io::copy(&mut response, &mut io::stdout());
+    if !response.status().is_success() {
+        error!("HTTP request failed: {}", response.status());
+        let mut buffer = String::new();
+        response.take(10000).read_to_string(&mut buffer).expect("Failed to read response");
+        error!("{}", buffer);
+    }
 }
