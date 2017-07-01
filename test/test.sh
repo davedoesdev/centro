ef149247c076fc884d795d7a32772af86c16edc5
David Halls <dahalls@gmail.com>
2017-07-01 09:32:51 +0100

    Run coveralls separately to see if that's holding up the build

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+set -e
+set -x
+
 ( while true; do echo keep alive!; sleep 60; done ) &
-if ! npm run ci-coverage-$(node -p 'process.versions.node.split(".")[0]') >& test.log; then
+
+node_ver=$(node -p 'process.versions.node.split(".")[0]')
+check_ver=6
+
+if [ $node_ver = $check_ver ]; then
+  grunt check_dist
+fi
+
+if ! npm run coverage >& test.log; then
   tail -n 1000 test.log
   exit 1
 fi
 tail -n 100 test.log
+
+if [ $node_ver = $check_ver ]; then
+  grunt coveralls
+fi
