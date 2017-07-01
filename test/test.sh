0f0404a92ee808748f250b0bea7d14745ef91d15
David Halls <dahalls@gmail.com>
2017-07-01 09:40:32 +0100

    Fix path to grunt in test.sh

--- test/test.sh
+++ test/test.sh
@@ -8,7 +8,7 @@ node_ver=$(node -p 'process.versions.node.split(".")[0]')
 check_ver=6
 
 if [ $node_ver = $check_ver ]; then
-  grunt check_dist
+  ./node_modules/.bin/grunt check_dist
 fi
 
 if ! npm run coverage >& test.log; then
@@ -18,5 +18,5 @@ fi
 tail -n 100 test.log
 
 if [ $node_ver = $check_ver ]; then
-  grunt coveralls
+  ./node_modules/.bin/grunt coveralls
 fi
