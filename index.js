e8726e1b01749659fd74e72192aa7da3f3897f0d
David Halls <dahalls@gmail.com>
2017-07-29 08:51:33 +0100

    Add summary to index

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /**
+ * Pub-sub and work queue server. Wildcards, streams, back-pressure,
+ * multi-transport. Just Node and a filesystem required.
+ *
  * Re-exports all exports of {@link centro-js/lib/client} and {@link centro-js/lib/server}
  * @module centro-js
  */
