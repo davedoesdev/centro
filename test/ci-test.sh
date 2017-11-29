e8969b783d12b1c20f2f8410cf13e130332937ce
David Halls <dahalls@gmail.com>
2017-11-29 22:47:48 +0000

    Circle CI is taking a long time, don't redirect logs

--- test/ci-test.sh
+++ test/ci-test.sh
@@ -11,11 +11,12 @@ if [ $node_ver = $check_ver ]; then
   ./node_modules/.bin/grunt check_dist
 fi
 
-if ! npm run coverage >& "$CIRCLE_ARTIFACTS/test.log"; then
-  tail -n 1000 "$CIRCLE_ARTIFACTS/test.log"
-  exit 1
-fi
-tail -n 100 "$CIRCLE_ARTIFACTS/test.log"
+#if ! npm run coverage >& "$CIRCLE_ARTIFACTS/test.log"; then
+#  tail -n 1000 "$CIRCLE_ARTIFACTS/test.log"
+#  exit 1
+#fi
+#tail -n 100 "$CIRCLE_ARTIFACTS/test.log"
+npm run coverage
 
 if [ $node_ver = $check_ver ]; then
   ./node_modules/.bin/grunt coveralls
