32c0835ae303185cbbaf2cabb9d287060bd8534b
David Halls <dahalls@gmail.com>
2018-01-02 22:27:26 +0000

    Export split topic env var

--- test/ci-test.sh
+++ test/ci-test.sh
@@ -11,12 +11,14 @@ if [ $node_ver = $check_ver ]; then
   ./node_modules/.bin/grunt check_dist
 fi
 
+# CircleCI runs on aufs which has max file name length 242
+export SPLIT_TOPIC_AT=150
+
 #if ! npm run coverage >& "$CIRCLE_ARTIFACTS/test.log"; then
 #  tail -n 1000 "$CIRCLE_ARTIFACTS/test.log"
 #  exit 1
 #fi
 #tail -n 100 "$CIRCLE_ARTIFACTS/test.log"
-SPLIT_TOPIC_AT=150 # CircleCI runs on aufs which has max file name length 242
 npm run coverage
 
 if [ $node_ver = $check_ver ]; then
