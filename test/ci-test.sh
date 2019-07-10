bd83e16a14c7683f11ff2ce2aa86d226cbb27821
David Halls <dahalls@gmail.com>
2019-07-10 22:24:01 +0100

    CircleCI: configure postgres client

--- test/ci-test.sh
+++ test/ci-test.sh
@@ -5,7 +5,7 @@ set -e
 set -x
 
 node_ver=$(node -p 'process.versions.node.split(".")[0]')
-check_ver=11
+check_ver=12
 
 if [ $node_ver = $check_ver ]; then
   ./node_modules/.bin/grunt check_dist
@@ -13,6 +13,8 @@ fi
 
 # CircleCI runs on aufs which has max file name length 242
 export SPLIT_TOPIC_AT=150
+
+npm explore qlobber-pg -- npm run migrate up
 
 #if ! npm run coverage >& "$CIRCLE_ARTIFACTS/test.log"; then
 #  tail -n 1000 "$CIRCLE_ARTIFACTS/test.log"
