08b3ff9f237f5800adb0efbd0a5ee583ca5f2b06
David Halls <dahalls@gmail.com>
2020-09-13 12:38:47 +0100

    Fix lint, dist, circleci pg image

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,11 @@ module.exports = {
         es6: true
     },
     parserOptions: {
-        ecmaVersion: 2018
+        ecmaVersion: 2018,
+        sourceType: "script"
     },
     extends: "eslint:recommended",
+    parser: "babel-eslint",
     rules: {
         "no-unused-vars": [
             "error", {
